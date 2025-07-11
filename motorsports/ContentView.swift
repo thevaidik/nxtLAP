1995d2b113329c62f735023a49937bbfb3cc0cfb
Vaidik <vaidik50000@gmail.com>
2025-07-11 20:59:44 +0530

    Initial Commit

---
+++ motorsports/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  motorsports
+//
+//  Created by Vaidik Dubey on 11/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ motorsports/motorsportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  motorsportsApp.swift
+//  motorsports
+//
+//  Created by Vaidik Dubey on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct motorsportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
